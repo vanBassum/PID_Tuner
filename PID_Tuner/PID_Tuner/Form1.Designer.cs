d1a9e23454a75011fb7df61e733254e67e0bf4f4
Bas Visscher <bas@koolecontrols.nl>
2019-09-18 16:20:57 +0200

    Basics working

--- PID_Tuner/PID_Tuner/Form1.Designer.cs
+++ PID_Tuner/PID_Tuner/Form1.Designer.cs
@@ -29,12 +29,45 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(583, 300);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
--- PID_Tuner/PID_Tuner/Form1.cs
+++ PID_Tuner/PID_Tuner/Form1.cs
@@ -7,14 +7,185 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace PID_Tuner
 {
     public partial class Form1 : Form
     {
+        
+
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            double fs = 100;
+            Source s = Source.Sine(1, fs);
+            FIR fir = new FIR();
+            PID pid = new PID();
+            Series ser = new Series();
+            ser.ChartType = SeriesChartType.Point;
+
+
+            pid.TS = 1 / fs;
+
+            fir.Reset();
+            s.Reset();
+            pid.Reset();
+            
+
+            
+
+            double fb = 0;
+
+            for(int i=0; i< fs; i++)
+            {
+
+                fb = fir.Cycle(pid.Cycle(fb, 2));
+                
+                ser.Points.AddXY(i, fb);
+            }
+            chart1.Series.Add(ser);
         }
     }
+
+
+    public class PID
+    {
+        public double KC { get; set; } = 0.5; // Controller gain
+        public double TI { get; set; } = 0.01; // Time-constant for I action
+        public double TD { get; set; } = 0; // Time-constant for D action
+        public double TS { get; set; } = 0; // Sample time [sec.]
+
+        double xk_1;  // PV[k-1] = Thlt[k-1]
+        double xk_2;  // PV[k-2] = Thlt[k-1]
+        double k0; // k0 value for PID controller
+        double k1; // k1 value for PID controller
+        double k2; // k2 value for PID controller
+        double k3; // k3 value for PID controller
+        double pp; // debug
+        double pi; // debug
+        double pd; // debug
+        double yk;
+
+
+        public void Init()
+        {
+            if (TI == 0.0)
+            {
+                k0 = 0.0;
+            }
+            else
+            {
+                k0 = (KC * TS) / TI;
+            } // else
+
+            k1 = (KC * TD) / TS;
+        }
+
+
+        public double Cycle(double xk, double tset)
+        {
+            pp = KC * (xk_1 - xk);              // Kc.(x[k-1]-x[k])
+            pi = k0 * (tset - xk);              // (Kc.Ts/Ti).e[k]
+            pd = k1 * (2.0 * xk_1 - xk - xk_2); // (Kc.Td/Ts).(2.x[k-1]-x[k]-x[k-2])
+            yk += pp + pi + pd;                 // add y[k-1] + P, I & D actions to y[k]
+
+            xk_2 = xk_1;                        // x[k-2] = x[k-1]
+            xk_1 = xk;                          // x[k-1] = x[k]
+
+            /*
+            // limit y[k] to GMA_HLIM and GMA_LLIM
+            if (yk > GMA_HLIM)
+            {
+                yk = GMA_HLIM;
+            }
+            else if (yk < GMA_LLIM)
+            {
+                yk = GMA_LLIM;
+            }
+            */
+
+            return yk;
+        }
+
+        public void Reset()
+        {
+            Init();
+        }
+
+
+
+    }
+
+
+
+
+    public class FIR
+    {       
+        public double[] Cooficients { get; set; } = new double[] {1};
+
+        private List<double> hist = new List<double>();
+
+        public double Cycle(double input)
+        {
+            double accum = 0;
+
+            hist.Insert(0, input);
+            if (hist.Count > Cooficients.Length)
+                hist.RemoveRange(Cooficients.Length, hist.Count - Cooficients.Length);
+
+            int to = Math.Min(Cooficients.Length, hist.Count());
+
+            for (int k = 0; k < to; k++)
+                accum += Cooficients[k] * hist[k];
+
+            
+
+            return accum;
+        }
+
+        public void Reset()
+        {
+            hist.Clear();
+        }
+    }
+
+
+
+
+    public class Source
+    {
+        int actualStep = 0;
+        Func<int, double> function;
+
+        public Source(Func<int, double> func)
+        {
+            function = func;
+        }
+
+        public double Cycle()
+        {
+            return function(actualStep++);
+        }
+
+        public void Reset()
+        {
+            actualStep = 0;
+        }
+
+
+
+        public static Source Step(double ampl = 1) => new Source(n => n > 0 ? ampl : 0);
+        public static Source Impulse(double ampl = 1) => new Source(n => n == 0 ? ampl : 0);
+        public static Source Ramp(double ampl = 1) => new Source(n => ampl * n);
+        public static Source Sine(double f, double fs, double ampl = 1) => new Source(n => ampl * Math.Sin(2 * Math.PI * f * n / fs));
+
+    }
+
+
 }
